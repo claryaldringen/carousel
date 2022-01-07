fd48cbae35f9df59873f3bcc075ac919f8a1bd86
Martin Zadražil <zadram1@gmail.com>
2022-01-07 22:19:24 +0100

    Publish config

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
+
+import pkg from './package.json'
+
+export default [
+  {
+    input: 'src/components/Carousel/index.js',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      postcss(),
+      resolve(),
+      babel({
+        presets: ['@babel/preset-react']
+      }),
+      commonjs()
+    ]
+  }
+]
